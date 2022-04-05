e80f277b7b3af7bee8ccc919979e3a9d781eb981
Dwerg01 <nardum@gmail.com>
2022-04-05 10:41:51 +0200

    First commit, all generic and placeholder items

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+// scroll to top funcitonality
+
+const scrollUp = document.querySelector("#scroll-up");
+
+scrollUp.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+
+    });
+});
