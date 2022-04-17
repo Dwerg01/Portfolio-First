64a161188967e4ff4721386989ab6c94fa701541
Dwerg01 <nardum@gmail.com>
2022-04-17 22:29:02 +0200

    carousel working for skills section

--- assets/js/app.js
+++ assets/js/app.js
@@ -35,3 +35,25 @@ link.addEventListener("click", () => {
     ul.classList.remove("show");
     menuArrow.classList.remove("rotate");
     }));
+
+    document.querySelector(".copywrite").textContent =
+    ` copyright ${(new Date().getFullYear())}`
+
+const Buttons = document.querySelectorAll(".carousel-button")
+
+Buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+        const slides = button.closest("[data-carousel]").querySelector("[data-slides]")
+
+        const activeSlide = slides.querySelector("[data-active]")
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+
+        if (newIndex < 0) newIndex = slides.children.length - 1;
+        if (newIndex >= slides.children.length) newIndex = 0;
+
+        slides.children[newIndex].dataset.active = true;
+        delete activeSlide.dataset.active
+        
+    })
+})
