67cdc8c1b455ee23c7e88433855377c9679c37ae
Dwerg01 <nardum@gmail.com>
2022-06-21 09:28:08 +0200

    projects updated on Swiper section, still owing skills section

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,16 +1,14 @@
 var swiper = new Swiper(".mySwiper", {
-	effect: "coverflow",
+	effect: "flip",
+	speed: 700,
+	slideShdows: false,
 	grabCursor: true,
-	centeredSlides: true,
-	slidesPerView: "auto",
-	coverflowEffect: {
-		rotate: 50,
-		stretch: 0,
-		depth: 100,
-		modifier: 1,
-		slideShadows: true,
-	},
+	rewind: true,
 	pagination: {
-		el: ".swiper-pagination",
+	  el: ".swiper-pagination",
 	},
-});
+	navigation: {
+	  nextEl: ".swiper-button-next",
+	  prevEl: ".swiper-button-prev",
+	},
+  });
